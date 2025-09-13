68db479f9988120b62df3852b181ef4c01c56ece
Élie Goudout <eliegoudout@hotmail.com>
2025-09-13 13:41:45 +0200

    enh: cleaner config loader, accepts yaml, raises on issue
    Signed-off-by: Élie Goudout <eliegoudout@hotmail.com>

--- cassiopeia/_configuration/load.py
+++ cassiopeia/_configuration/load.py
@@ -1,26 +1,32 @@
-from typing import Dict
-import os
+from pathlib import Path
+import json
 
-try:
-    import ujson as json
-except ImportError:
-    import json
+from ruamel.yaml import YAML
 
 
-def load_config(filename=None) -> Dict:
-    head, tail = os.path.split(__file__)
+def load_config(filename: Path | str | None = None) -> dict[str, object]:
+    """Load config file from YAML or JSON.
 
-    if filename is not None:
-        if not os.path.exists(filename):
-            filename = os.path.normpath(os.path.join(head, filename))
-            if not os.path.exists(filename):
-                print(
-                    "WARNING! Could not find settings file {}, using default.".format(
-                        filename
-                    )
-                )
+    Looks for absolute path and retries relatively to this file (avoid
+    breaking change).
+    """
+    if filename is None:
+        return {}
 
-        config = json.loads(open(filename).read())
-    else:
-        config = {}
-    return config
+    # Check file exists
+    path = Path(filename)
+    if not path.exists():
+        path = Path(__file__).parent / path.name
+        if not path.exists():
+            msg = f"Could not find settings file for {filename!r}"
+            raise FileNotFoundError(msg)
+
+    # Load content
+    ext = path.suffix.lower()[1:]
+    if ext == "json":
+        return json.loads(path.read_text(encoding="utf-8"))
+    if ext in {"yaml", "yml"}:
+        return YAML(typ='safe').load(path)
+
+    msg = f"Unsupported settings file extension {ext!r}"
+    raise ValueError(msg)
